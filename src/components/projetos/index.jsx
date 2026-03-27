282ccd1b95225f9615aa80f5dce13e8106c1e67f
rodrigojordison <rodrigojordisonsilva@gmail.com>
2026-03-27 16:17:52 -0300

    feat: add project cards with images and GitHub links to Projetos section

--- src/components/projetos/index.jsx
+++ src/components/projetos/index.jsx
@@ -1,15 +1,39 @@
-import style from './projetos.module.css';
+import style from "./projetos.module.css";
+import { FaGithub } from "react-icons/fa";
 
 export default function Projetos() {
   return (
     <>
-       <section id="projetos" className={style.projetosContainer}>
-         <h2 data-aos="fade-right">Meus Projetos</h2>
-         <div className={style.projetosContent}>
-           <p data-aos="fade-left">Em breve...</p>
-           <span data-aos="fade-up">Novos Projetos estão sendo desenvolvidos!</span>
-         </div>
-       </section>
+      <section id="projetos" className={style.projetosContainer}>
+        <h2 data-aos="fade-right">Meus Projetos</h2>
+        <div className={style.projetosContent}>
+          <div className={style.projetoCard} data-aos="fade-up">
+            <img src="image/fotos-projetos/quimica.jpg" alt="Descrição da imagem" />
+            <div className={style.links}>
+              <a
+                href="https://github.com/seu-usuario/nome-do-projeto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+
+          <div className={style.projetoCard} data-aos="fade-up">
+            <img src="image/fotos-projetos/mayara.jpg" alt="Descrição da imagem" />
+            <div className={style.links}>
+              <a
+                href="https://github.com/seu-usuario/nome-do-projeto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
